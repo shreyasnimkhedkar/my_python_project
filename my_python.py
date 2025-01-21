20be2866d935c4d17170424a8966281a4d34de44
shreyasnimkhedkar <shreyasgithub1996@gmail.com>
2025-01-21 09:47:14 +0530

    Update and rename streamlit_app.py to my_python.py

---
+++ my_python.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from streamlit_card import card
+
+
+
+
+
+st.title("PYTHON PROJECT")
+
+st.write('''
+This is a Python-based Data Analysis Project designed to provide powerful insights and visualizations, leveraging the 
+flexibility and interactivity of Streamlit. The application is aimed at simplifying the data exploration and analysis process 
+for users, offering real-time results and actionable insights.
+''')
+
+
+card(
+    title="Hotel Management Project",
+    text="click this card for project",
+    url = "https://hotelpy.streamlit.app/"
+)
